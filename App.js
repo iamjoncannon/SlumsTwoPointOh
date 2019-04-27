e2e5cc51eae08ffb5e9d05a874d24d70583335b0
Kenneth Duverge <duvergekenneth@gmail.com>
2019-04-27 13:16:13 -0400

    MongoDB Stitch SDK React-Native boilerplate setup

--- App.js
+++ App.js
@@ -1,62 +1,82 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-react-native-sdk';
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUserId: undefined,
+      client: undefined
+    };
+    this._loadClient = this._loadClient.bind(this);
+    this._onPressLogin = this._onPressLogin.bind(this);
+    this._onPressLogout = this._onPressLogout.bind(this);
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+  componentDidMount() {
+    this._loadClient();
+  }
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
+  render() {
+    let loginStatus = "Currently logged out."
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+    if (this.state.currentUserId) {
+      loginStatus = `Currently logged in as ${this.state.currentUserId}!`
+    }
+
+    loginButton = <Button
+      onPress={this._onPressLogin}
+      title="Login" />
+
+    logoutButton = <Button
+      onPress={this._onPressLogout}
+      title="Logout" />
+
+    return (
+      <View style={styles.container}>
+        <Text> {loginStatus} </Text>
+        {this.state.currentUserId !== undefined ? logoutButton : loginButton}
+      </View>
+    );
+  }
+
+  _loadClient() {
+    Stitch.initializeDefaultAppClient('slumsapp-qbxrr').then(client => {
+      this.setState({ client });
+
+      if (client.auth.isLoggedIn) {
+        this.setState({ currentUserId: client.auth.user.id })
+      }
+    });
+  }
+
+  _onPressLogin() {
+    this.state.client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+      console.log(`Successfully logged in as user ${user.id}`);
+      this.setState({ currentUserId: user.id })
+    }).catch(err => {
+      console.log(`Failed to log in anonymously: ${err}`);
+      this.setState({ currentUserId: undefined })
+    });
+  }
+
+  _onPressLogout() {
+    this.state.client.auth.logout().then(user => {
+      console.log(`Successfully logged out`);
+      this.setState({ currentUserId: undefined })
+    }).catch(err => {
+      console.log(`Failed to log out: ${err}`);
+      this.setState({ currentUserId: undefined })
+    });
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
